8c9e83dde3bb226737ed42be30b71ad4c44e202e
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2021-12-12 17:19:13 +0200

    add getMovies UseCase

---
+++ features/home/src/main/java/com/mahmoud_ashraf/home/domain/usecases/GetMoviesUseCase.kt
@@ -0,0 +1,11 @@
+package com.mahmoud_ashraf.home.domain.usecases
+
+import com.mahmoud_ashraf.home.data.model.MoviesResponse
+import com.mahmoud_ashraf.home.domain.MoviesRepository
+import javax.inject.Inject
+
+class GetMoviesUseCase@Inject constructor(val repository: MoviesRepository) {
+
+    suspend operator fun invoke(pageNumber: Int): MoviesResponse = repository.getMovies(pageNumber)
+
+}
--- features/home/src/main/java/com/mahmoud_ashraf/home/presentation/HomeViewModel.kt
+++ features/home/src/main/java/com/mahmoud_ashraf/home/presentation/HomeViewModel.kt
@@ -5,17 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mahmoud_ashraf.home.domain.MoviesRepository
+import com.mahmoud_ashraf.home.domain.usecases.GetMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(val repository: MoviesRepository) : ViewModel() {
+class HomeViewModel @Inject constructor(val getMoviesUseCase: GetMoviesUseCase) : ViewModel() {
 
     private val _message = MutableLiveData("Hi")
     val message: LiveData<String> = _message
 
     fun fetch()= viewModelScope.launch {
-        repository.getMovies(12)
+        getMoviesUseCase(10)
     }
 }
